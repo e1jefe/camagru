690fdcac7955f7c8e1dc7dd0b2e00cbe89e93fa6
e1jefe <dmitry.sheptun@gmail.com>
2018-04-23 17:32:16 +0300

    snapshot

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Camagru</title>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
@@ -20,28 +20,16 @@
         </div>
         <div class="btn-folder">
             <a>
-                    <label for="file-input">
-                        <img src="/img/catalog.png"/>
-                    </label>
+                <label for="file-input">
+                    <img src="/img/catalog.png"/>
+                </label>
 
-                    <input id="file-input" style="display:none;" type="file"/>
-                </a>
+                <input id="file-input" style="display:none;" type="file"/>
+            </a>
         </div>
     </div>
     <div class="middle">
         <div class="hovergallery">
-            <img src="/img/1a.jpg">
-            <img src="/img/2a.jpg">
-            <img src="/img/3a.jpg">
-            <img src="/img/4a.jpg">
-            <img src="/img/5a.jpg">
-            <img src="/img/6a.jpg">
-            <img src="/img/1a.jpg">
-            <img src="/img/2a.jpg">
-            <img src="/img/3a.jpg">
-            <img src="/img/4a.jpg">
-            <img src="/img/5a.jpg">
-            <img src="/img/6a.jpg">
             <img src="/img/1a.jpg">
             <img src="/img/2a.jpg">
             <img src="/img/3a.jpg">
@@ -53,6 +41,44 @@
 
     <div class="right"></div>
 </div>
+<video autoplay muted></video>
+<script>
+    var photo = document.getElementsByClassName('btn-photo');
+    photo[0].addEventListener('click', function () {
+        {
+            if (navigator.webkitGetUserMedia != null) {
+                var options = {
+                    video: true,
+                    audio: true
+                };
+// запрашиваем доступ к веб-камере
+                navigator.webkitGetUserMedia(options,
+                    function (stream) {
+// получаем тег video
+                        var video = document.querySelector('video');
+// включаем поток в магический URL
+                        video.src = window.webkitURL.createObjectURL(stream);
+                    },
+                    function () {
+                        console.log("error happened");
+                    }
+                );
+            }
+        }
+    })
+</script>
+<form ><input type='button' id='snapshot' value="snapshot"></form>
+
+<canvas id='canvas' width='640' height='480' ></canvas>
+
+<script>
+    document.getElementById('snapshot').onclick = function() {
+        var video = document.querySelector('video');
+        var canvas = document.getElementById('canvas');
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(video,0,0,640,480);
+    }
+</script>
 <footer class="container">
     <div class="copyrights">
         <p>
@@ -60,7 +86,7 @@
         </p>
     </div>
     <div class="btn-home">
-        <a href="#home" onclick="up()">
+        <a onclick="up()">
             <img src="img/arrow.png">
         </a>
     </div>
@@ -79,34 +105,6 @@
         else
             clearTimeout(t);
     }
-</script>
-
-<video id="localVideo" autoplay muted></video>
-<script>
-    var photo = document.getElementsByClassName('btn-photo');
-    photo[0].addEventListener('click', function () {
-        var PeerConnection = window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-        navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
-
-        navigator.getUserMedia(
-            { audio: true, video: true },
-            gotStream,
-            function(error) { console.log(error) }
-        );
-
-        function gotStream(stream) {
-            var okno = document.getElementById("localVideo");
-            okno.style.display = "block";
-            okno.src = URL.createObjectURL(stream);
-
-            pc = new PeerConnection(null);
-            pc.addStream(stream);
-            pc.onicecandidate = gotIceCandidate;
-            pc.onaddstream = gotRemoteStream;
-        }
-    })
-
-
 </script>
 
 </body>
