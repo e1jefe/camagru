075a46d8c951b19b859e1c34e6a422e55c0b9e4f
e1jefe <dmitry.sheptun@gmail.com>
2018-05-23 19:39:04 +0300

    add change pass via email

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -185,45 +185,30 @@ class UserController extends Controller
                 header("Location: login"); exit();
             }
         }
-        else if(isset($_GET['email']))
-        {
-
-
-        }
-
     }
-//    public function changepassmailAction()
-//    {
-//        $this->view->render('changepassmail');
-//        $connection = new Db;
-//        var_dump($_POST);
-//        echo "<br>after post";
-//        if(isset($_POST['submit'])){
-//            echo "<p>This is GET</p>";
-//            var_dump($_GET);
-//            $email = $_GET['email'];
-//            $token = $_GET['token'];
-//
-//            $pass = (hash('whirlpool', ($_POST['pass'])));
-//            $newpass = (hash('whirlpool', ($_POST['newpass'])));
-//                $res = $connection->row("SELECT * FROM users WHERE user_email= '$email'");
-//                echo "<br>";
-//                var_dump($res);
-//        if ($res != null) {
-//            if ($res[0]['user_token'] == $token && $res[0]['email'] == $email) {
-//                var_dump($res[0]['user_token']);
-//                var_dump($res[0]['email']);
-//                if ($pass == $newpass) {
-//                    $connection->query("UPDATE users SET user_password='$pass' WHERE email= '$email'");
-//                } else {
-//                    echo "<script>alert(\"Password don't match\");</script>";
-//                }
-//            }
-//            echo "<script>alert(\"Password changed\");</script>";
-//            header("Refresh:2; login");
-//            exit();
-//        }
-//                 echo "<script>alert(\"Enter password\");</script>";
-//    }
-//    }
+    public function changepassmailAction()
+    {
+        $this->view->render('changepassmail');
+        $connection = new Db;
+        if(isset($_POST['submit'])){
+            $email = $_POST['email'];
+            $token = $_POST['token'];
+            $pass = (hash('whirlpool', ($_POST['pass'])));
+            $newpass = (hash('whirlpool', ($_POST['newpass'])));
+            $res = $connection->row("SELECT * FROM users WHERE user_token= '$token'");
+        if ($res != null) {
+            if ($res[0]['user_token'] == $token && $res[0]['email'] == $email) {
+                if ($pass == $newpass) {
+                    $connection->query("UPDATE users SET user_password='$pass' WHERE email= '$email'");
+                } else {
+                    echo "<script>alert(\"Password don't match\");</script>";
+                }
+            }
+            echo "<script>alert(\"Password changed\");</script>";
+            header("Refresh:2; login");
+            exit();
+        }
+                 echo "<script>alert(\"Enter password\");</script>";
+    }
+    }
 }
--- views/user/changepassmail.php
+++ views/user/changepassmail.php
@@ -1,3 +1,4 @@
+
 <section class="changepassmail">
     <div class="form">
         <form class="register-form" action="changepassmail" method="post">
@@ -6,8 +7,24 @@
             </p>
             <input type="password" name="pass" placeholder="enter password" value=""/>
             <input type="password" name="newpass" placeholder="confirm password" value=""/>
+            <input type="hidden" name="email" value="" />
+            <input type="hidden" name="token" value="" />
             <button name="submit" value="OK" />Submit</button>
 <!--            <p class="message">Already registered? <a href="login">Sign In</a></p>-->
         </form>
     </div>
 </section>
+    <script>
+        document.addEventListener("DOMContentLoaded", function (e) {
+            console.log("here");
+            let url = new URL(window.location.href);
+            let email = url.searchParams.get("email");
+            let token = url.searchParams.get("token");
+            console.log("email:", email, "token:", token);
+            let emailInput = document.getElementsByName("email")[0];
+            let tokenInput =  document.getElementsByName("token")[0];
+            console.log("emailInput:", emailInput, "tokenInput:", tokenInput);
+            emailInput.value = email;
+            tokenInput.value = token;
+        });
+    </script>
