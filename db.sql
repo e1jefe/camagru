7d1175829429a700eae7bb9a97b97ac9af12ca92
e1jefe <dmitry.sheptun@gmail.com>
2018-06-17 16:27:15 +0300

    htaccess

--- db.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `posts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` text NOT NULL,
-  `description` text NOT NULL,
-  `text` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
